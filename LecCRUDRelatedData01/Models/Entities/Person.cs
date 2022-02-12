ccb289413f8c721707ce2e2a4815dd5bc434d851
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2022-02-12 11:48:56 -0500

    Add project files.

---
+++ LecCRUDRelatedData01/Models/Entities/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LecCRUDRelatedData01.Models.Entities;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string UserName { get; set; } = String.Empty;
+
+    [StringLength(256)]
+    public string? FirstName { get; set; }
+
+    [StringLength(256)]
+    public string? MiddleName { get; set; }
+
+    [Required]
+    [StringLength(256)]
+    public string LastName { get; set; } = String.Empty;
+
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+
+    public ICollection<Recommendation> Recommendations { get; set; }
+        = new List<Recommendation>();
+}
+
---
+++ LecCRUDRelatedData01/Models/Entities/Recommendation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LecCRUDRelatedData01.Models.Entities;
+
+public class Recommendation
+{
+    public int Id { get; set; }
+
+    public int Rating { get; set; }
+
+    [StringLength(512)]
+    public string Narrative { get; set; } = String.Empty;
+
+    public int PersonId { get; set; }
+    public Person? Person { get; set; }
+}
+
